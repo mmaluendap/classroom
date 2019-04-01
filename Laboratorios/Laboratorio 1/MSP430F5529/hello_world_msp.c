6f972ce15cdbbbd5b717c0f17016f998d619e960
Felipe Sanchez <fssanchez@uc.cl>
2019-03-31 23:15:00 -0300

    Fix bug in code for Code Composer Studio

--- Laboratorios/Laboratorio 1/MSP430F5529/hello_world_msp.c
+++ Laboratorios/Laboratorio 1/MSP430F5529/hello_world_msp.c
@@ -1,5 +1,4 @@
 #include <msp430.h>
-#include <stdint.h>
 
 int main(void)
 {
@@ -7,10 +6,12 @@ int main(void)
     WDTCTL = WDTPW | WDTHOLD;  //Stop watchdog timer, it runs by default.
     P1DIR |= 0x01;             //Port 1 direction register, Pin0 as output
 
+    volatile unsigned int i;
+
     while (1) //Infinite loop
     {
         P1OUT ^= 0x01; // Toggle P1.0 (XOR)
-   for(volatile uint32_t i=0; i< 10000; i++); // Busy wait delay
+   		for(i = 10000; i>0; i--); // Busy wait delay
     }
 
     return 0;
